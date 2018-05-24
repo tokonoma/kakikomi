6367c5890ed7e748ba67008ba3065f5057aa5d0b
Reeder, Tim <tReeder@bna.com>
2018-05-24 14:07:42 -0400

    base styling for write complete, to do - tag iteraction and all of posts

--- posts.php
+++ posts.php
@@ -19,29 +19,20 @@
 	<div class="container">
 		<div class="row">
 			<div class="col-6 col-sm-4 offset-sm-2">
-				<a href="posts.php" class="tab text-center">posts</a>
+				<a href="posts.php" class="tab text-center active">posts</a>
 			</div>
 			<div class="col-6 col-sm-4">
-				<a href="write.php" class="tab text-center active">write</a>
+				<a href="write.php" class="tab text-center">write</a>
 			</div>
 		</div>
 	</div>
 </nav>
 
-<main class="kakikomi-write">
+<main class="kakikomi-posts">
 	<div class="container">
 		<div class="row">
 			<div class="col-12 col-sm-8 offset-sm-2">
-				<input type="text" id="title-input" class="title-input" aria-label="title input" placeholder="add a title for your post">
-				<input class="invisible-input post-date" value="<?php echo date("M • d • Y"); ?>" placeholder="add a publish date">
-				<textarea id="post-input" class="post-input" rows="3" placeholder="write your post here"></textarea>
-				<div class="action-bar">
-					<a href="" class="btn btn-link clear secondary">CLEAR</a>
-					<div class="float-right">
-						<button type="button" class="btn btn-link secondary">SAVE DRAFT <span class="badge">5 • 24</span></button>
-						<button type="button" class="btn btn-link submit">PUBLISH</button>
-					</div>
-				</div>
+				<!-- item -->
 			</div>
 		</div>
 	</div>
